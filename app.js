080e90837d2296bb0f838feb8c32cf4cdb0595bc
Guilherme Avila <106833885+TerryKrom@users.noreply.github.com>
2023-05-13 19:14:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let inputval = document.querySelector('#cidade-in')
 let btn = document.querySelector('#add')
-let city = document.querySelector('#cidade-out')
 let descricao = document.querySelector('#descricao')
 let temp = document.querySelector('#temp')
 let vento = document.querySelector('#vento')
+let display = document.getElementById('display')
 let apik = 'f17758115b7520522e0f1a7f9a7e3159'
 //acessando todos os inputs e a API key da API que será utilizada
 
@@ -67,10 +67,20 @@ const info = () => {
         let temperatura = data['main']['temp']
         let v_vento = data['wind']['speed']
         let descrip_pt = translate(descrip)
-        city.innerHTML=`Cidade: ${name}`
-        temp.innerHTML=`Temperatura: ${conversao(temperatura)}°C`
-        descricao.innerHTML=`Condições: ${descrip_pt}`
-        vento.innerHTML=`Velocidade do Vento: ${v_vento} KM/H`
+        temp.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-thermometer" viewBox="0 0 16 16">
+        <path d="M8 14a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+        <path d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0zM6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15V2.5z"/>
+      </svg>
+      ${conversao(temperatura)}°C`
+        descricao.innerHTML=`&nbsp;&nbsp; <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-cloud-sun" viewBox="0 0 16 16">
+        <path d="M7 8a3.5 3.5 0 0 1 3.5 3.555.5.5 0 0 0 .624.492A1.503 1.503 0 0 1 13 13.5a1.5 1.5 0 0 1-1.5 1.5H3a2 2 0 1 1 .1-3.998.5.5 0 0 0 .51-.375A3.502 3.502 0 0 1 7 8zm4.473 3a4.5 4.5 0 0 0-8.72-.99A3 3 0 0 0 3 16h8.5a2.5 2.5 0 0 0 0-5h-.027z"/>
+        <path d="M10.5 1.5a.5.5 0 0 0-1 0v1a.5.5 0 0 0 1 0v-1zm3.743 1.964a.5.5 0 1 0-.707-.707l-.708.707a.5.5 0 0 0 .708.708l.707-.708zm-7.779-.707a.5.5 0 0 0-.707.707l.707.708a.5.5 0 1 0 .708-.708l-.708-.707zm1.734 3.374a2 2 0 1 1 3.296 2.198c.199.281.372.582.516.898a3 3 0 1 0-4.84-3.225c.352.011.696.055 1.028.129zm4.484 4.074c.6.215 1.125.59 1.522 1.072a.5.5 0 0 0 .039-.742l-.707-.707a.5.5 0 0 0-.854.377zM14.5 6.5a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
+      </svg>
+      ${descrip_pt}`
+        vento.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
+        <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+      ${v_vento} KM/H`
     })
     .catch(err => alert('Cidade não encontrada!'))
 }
@@ -92,16 +102,24 @@ const daytime = () => {
     let element = document.querySelector('.container');
     let title = document.querySelector('.title')
     let data = new Date
-    let horas = data.getHours()
+    let horas = 20
     if(horas >= 5 && horas < 17){
         title.style.color='#e6e607'
         element.style.backgroundImage='url(./day.jpg)'
     }else if(horas >= 17 && horas < 20){
+        inputval.style.borderColor="#7e0e3b"
+        btn.style.borderColor="#7e0e3b"
+        btn.style.color="#7e0e3b"
+        display.style.borderColor="#7e0e3b"
         title.style.color='#fff'
         element.style.backgroundImage='url(./evening.jpg)'
     }
     else{
-        title.style.color='#ffffff'
+        inputval.style.borderColor="#8b1f99"
+        btn.style.borderColor="#8b1f99"
+        btn.style.color="#8b1f99"
+        display.style.borderColor="#8b1f99"
+        title.style.color='#fff'
         element.style.backgroundImage='url(./night.png)'
     }
 }
